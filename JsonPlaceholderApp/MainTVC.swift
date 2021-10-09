d390b4d2f78b5cde807d9593707fece4e016e684
atata174 <razhapov_albert@mail.ru>
2021-10-09 21:01:15 +0300

    first commit

---
+++ JsonPlaceholderApp/MainTVC.swift
@@ -0,0 +1,25 @@
+//
+//  MainTVC.swift
+//  JsonPlaceholderApp
+//
+//  Created by PenguinRaja on 09.10.2021.
+//
+
+import UIKit
+
+class MainTVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
